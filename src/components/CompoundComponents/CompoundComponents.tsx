7b33eb7f11d795f7e3d08340401d7509e3f0f6a2
Andres Badillo <r.andres.badillo@gmail.com>
2024-11-10 17:01:17 -0500

    clase 4 - Compound Component en React

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,17 @@
+import Tab from "./Tab";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <div>
+      <Tab label="Tigre">
+        🐯
+      </Tab>
+      <Tab label="Ratón">
+        🐭
+      </Tab>
+      <Tab label="Girafa">
+        🦒
+      </Tab>
+    </div>
   );
 };
