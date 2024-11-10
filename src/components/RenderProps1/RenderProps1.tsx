7b6bd1e9f542e5542986366d07ab62d63063741c
Andres Badillo <r.andres.badillo@gmail.com>
2024-11-10 16:16:31 -0500

    clase 2 - Render Props y Composicion

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,21 @@
+import React from "react";
+
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element;
+};
+
+export const ChildComponent: React.FC<ChildComponentProps> = ({ render }) => {
+  const name = "Andrés Badillo";
+  return (
+    <div>
+      {render(name)}
+    </div>
+  );
+};
+
 export const ParentComponent = () => {
   return (
-    <>
-    </>
+    <ChildComponent render={(name) => <p>Hello, {name}!!</p>}>
+    </ChildComponent>
   );
 };
